0775059f7dff6479da5a7824f5eff815b5997352
Maram99Alhinai <physics444maram@gmail.com>
2024-09-25 15:53:39 +0400

    update readme, update whatsapp_utils.pyaccept reqest to all phone numbers, delet start,py

--- README.md
+++ README.md
@@ -4,9 +4,58 @@ This project is chat bot that uses LLM. This project was crated to be shared for
 Python 3.11.4 
 
 Set_up steps :
-- python -m venv omanexplor
-- omanexplor\Scripts\activate
-- pip install -r requirements.txt
+- download
+  https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-windows-amd64.zip
+- Run the following command in ngrok
+ ```  ngrok http 8000 --domain=adapting-moccasin-vastly.ngrok-free.app ```
+- ``` git colne "https://github.com/Maram99Alhinai/Oman_Explorer.git" ```
+- ``` python -m venv omanexplor ```
+- ``` omanexplor\Scripts\activate ```
+- ``` pip install -r requirements.txt ```
 
 Steps to run:
-- Python main.py
+- ``` Python main.py ```
+
+
+### Directory
+Oman_Explorer/
+
+│
+
+├── app/
+
+│   ├── init.py
+
+│   ├── config.py
+
+│   ├── view.py
+
+│   ├── decorators/
+
+│   │   ├── security.py
+
+│   ├── services/
+
+│   │   ├── openai_service.py
+
+│   │   └── db_service.py
+
+│   ├── utils/
+
+│   │   ├── init.py
+
+│   │   ├── whatsapp_utils.py
+
+│
+
+├── requirements.txt
+
+├── .env                      
+
+├── .gitignore
+
+├── main.py
+
+├── README.md
+
+└── LICENSE
--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -24,11 +24,6 @@ def get_text_message_input(recipient, text):
     )
 
 
-# def generate_response(response):
-#     # Return text in uppercase
-#     return response.upper()
-
-
 def send_message(data):
     headers = {
         "Content-type": "application/json",
@@ -40,37 +35,25 @@ def send_message(data):
     try:
         response = requests.post(
             url, data=data, headers=headers, timeout=10
-        )  # 10 seconds timeout as an example
-        response.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+        )
+        response.raise_for_status()
     except requests.Timeout:
         logging.error("Timeout occurred while sending message")
         return jsonify({"status": "error", "message": "Request timed out"}), 408
-    except (
-        requests.RequestException
-    ) as e:  # This will catch any general request exception
+    except requests.RequestException as e:
         logging.error(f"Request failed due to: {e}")
         return jsonify({"status": "error", "message": "Failed to send message"}), 500
     else:
-        # Process the response as normal
         log_http_response(response)
         return response
 
 
 def process_text_for_whatsapp(text):
-    # Remove brackets
     pattern = r"\【.*?\】"
-    # Substitute the pattern with an empty string
     text = re.sub(pattern, "", text).strip()
-
-    # Pattern to find double asterisks including the word(s) in between
     pattern = r"\*\*(.*?)\*\*"
-
-    # Replacement pattern with single asterisks
     replacement = r"*\1*"
-
-    # Substitute occurrences of the pattern with the replacement
     whatsapp_style_text = re.sub(pattern, replacement, text)
-
     return whatsapp_style_text
 
 
@@ -81,21 +64,14 @@ def process_whatsapp_message(body):
     message = body["entry"][0]["changes"][0]["value"]["messages"][0]
     message_body = message["text"]["body"]
 
-    # TODO: implement custom function here
-    # response = generate_response(message_body)
-
-    # OpenAI Integration
     response = generate_response(message_body, wa_id, name)
     response = process_text_for_whatsapp(response)
 
-    data = get_text_message_input(current_app.config["RECIPIENT_WAID"], response)
+    data = get_text_message_input(wa_id, response)
     send_message(data)
 
 
 def is_valid_whatsapp_message(body):
-    """
-    Check if the incoming webhook event has a valid WhatsApp message structure.
-    """
     return (
         body.get("object")
         and body.get("entry")
